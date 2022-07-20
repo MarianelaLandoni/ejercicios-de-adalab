5f0bfc4050cf60a130b7361aea0e9e28d04bc10e
Marianela Landoni Herrera <mlandonih@hotmail.com>
2022-07-20 12:49:28 +0200

    Add hello function to button

--- modulo-2-leccion-13.2-chihuahuas/js/main.js
+++ modulo-2-leccion-13.2-chihuahuas/js/main.js
@@ -1,14 +1,21 @@
-"use strict";
+'use strict';
 
 function getChihuahua() {
-  fetch("https://dog.ceo/api/breed/chihuahua/images/random")
+  fetch('https://dog.ceo/api/breed/chihuahua/images/random')
     .then((response) => response.json())
     .then((data) => {
-      const img = document.querySelector("img");
+      const img = document.querySelector('img');
       img.src = data.message;
-      img.alt = "Un chichuhua";
-      img.title = "Un chihuahua";
+      img.alt = 'Un chichuhua';
+      img.title = 'Un chihuahua';
     });
 }
-const btn = document.querySelector(".js_dog");
-btn.addEventListener("click", getChihuahua);
+const btn = document.querySelector('.js_dog');
+btn.addEventListener('click', getChihuahua);
+
+function handleClickBtn() {
+  console.log('hola');
+}
+
+const clickMe = document.querySelector('.js_clickMe');
+clickMe.addEventListener('click', handleClickBtn);
